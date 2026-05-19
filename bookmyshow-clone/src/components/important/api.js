e2baf684fb3b888f2474f39adb64b5750ab2e421
Shouvik Sarkar <shouviksarkar2001@gmail.com>
2026-05-19 16:48:47 +0530

    Initial commit

---
+++ bookmyshow-clone/src/components/important/api.js
@@ -0,0 +1,9 @@
+export const apiUrl = {
+    
+    key : "9b25753bfc8c44fdb0023e172d66da6b",
+    base : "https://api.themoviedb.org/3/",
+    getGenres : "genre/movie/list",
+    nowPlaying : "movie/now_playing",
+    language : "language=en-US",
+    imageBase : "https://image.tmdb.org/t/p/w500"
+}
